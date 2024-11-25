9a3fc94336c9daa0ce92781906338196f07e4d52
Mongezi Kasha <mongezikasha@gmail.com>
2024-11-25 07:36:47 +0200

    Add project files.

---
+++ 2130_BLOGAPP/Models/Domain/BlogPost.cs
@@ -0,0 +1,15 @@
+﻿namespace _2130_BLOGAPP.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id{ get; set; }
+        public string Title { get; set; }
+        public string shortDescription { get; set; }
+        public string Content {  get; set; }
+        public string FeatureUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
---
+++ 2130_BLOGAPP/Models/Domain/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace _2130_BLOGAPP.Models.Domain
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+    }
+}
